71605cdebd530c90529f2e0f864206974d776d9e
Yves Faems <yves.faems@gmail.com>
2021-06-27 14:23:49 -0700

    user input validation

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -10,7 +10,12 @@ const AddUser = props => {
 
     const onSubmitHandler = (event) => {
         event.preventDefault();
-        console.log(addUserName, addUserAge)
+        if (addUserName.trim().length < 1 || addUserAge.trim().length < 1 || +addUserAge < -1) {
+            return
+        }      
+        console.log(addUserName.trim(), +addUserAge)
+        setAddUserName('');
+        setAddUserAge('');
     }
 
     const userNameChangeHandler = event => {
@@ -28,12 +33,14 @@ const AddUser = props => {
                 </label>
                 <input id='username'
                 type='text'
-                onChange={userNameChangeHandler} />
+                onChange={userNameChangeHandler}
+                value={addUserName} />
                 <label htmlFor='userage'>User age?
                 </label>
                 <input id='userage'
                 type='number'
-                onChange={userAgeChangeHandler} />
+                onChange={userAgeChangeHandler} 
+                value={addUserAge}/>
                 <Button type='submit'>Add User</Button>
             </form>
         </Card>
